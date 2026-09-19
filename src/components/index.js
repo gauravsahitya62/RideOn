2b3bab6ba93fc3d5f022fe59c41126a65161288a
gauravsahitya62 <gauravsahitya62@gmail.com>
2026-09-20 02:24:10 +0530

    feat: export Prompt 8 checkout components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,9 @@
-// Re-export the focused Prompt 7 components from one module for simpler screen imports.
+// Re-export the focused reusable booking/location components.
 export { default as LocationSelector } from './LocationSelector';
 export { default as AddressForm } from './AddressForm';
 export { default as SavedAddressPicker } from './SavedAddressPicker';
 export { default as DeliverySelector } from './DeliverySelector';
+export { default as CheckoutSummary } from './CheckoutSummary';
+export { default as BookingReview } from './BookingReview';
+export { default as PaymentMethodSelector } from './PaymentMethodSelector';
+export { default as BookingConfirmation } from './BookingConfirmation';
