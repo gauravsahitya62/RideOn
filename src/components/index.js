c095a6235003371661ec666b7babc8faf0e4a286
gauravsahitya62 <gauravsahitya62@gmail.com>
2026-09-20 02:10:22 +0530

    chore: export Prompt 7 location components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+// Re-export the focused Prompt 7 components from one module for simpler screen imports.
+export { default as LocationSelector } from './LocationSelector';
+export { default as AddressForm } from './AddressForm';
+export { default as SavedAddressPicker } from './SavedAddressPicker';
+export { default as DeliverySelector } from './DeliverySelector';
