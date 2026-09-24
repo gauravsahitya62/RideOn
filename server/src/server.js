74cbc69b2a0af3f051d77318210cea3a253858d5
gauravsahitya62 <gauravsahitya62@gmail.com>
2026-09-25 00:04:46 +0530

    fix: generate OTPs with cryptographic randomness

--- server/src/server.js
+++ server/src/server.js
@@ -222,7 +222,7 @@ function normalizeOtpDestination({ channel, value }) {
 }
 
 function generateOtpCode() {
-  return String(Math.floor(100000 + Math.random() * 900000));
+  return String(crypto.randomInt(100000, 1000000));
 }
 
 function hashOtpCode(code) {
